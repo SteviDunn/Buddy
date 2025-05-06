064fd6c4f5c35348b261c31171704fd80d8c85a7
SteviDunn <81661702+SteviDunn@users.noreply.github.com>
2025-05-06 13:24:22 -0700

    Fix email reply to field and add logo

--- app/api/buddy-signup/route.ts
+++ app/api/buddy-signup/route.ts
@@ -93,6 +93,8 @@ export async function POST(req: Request) {
       await resend.emails.send({
         from: 'Aubri <hello@aubri.site>',
         to: [currentUser.contact, buddy.contact],
+        replyTo: `${currentUser.contact}, ${buddy.contact}`,
+        bcc: ['yourcopy@aubri.site'],
         subject: `You're matched for ${eventTitle}!`,
         html:`
             <p>Hi ${currentUser.name} and ${buddy.name}!</p>
